1f0afaf2e9db4bc9c888451254b6954ddea25d14
PeterWang-dev <PeterWang030908@hotmail.com>
2024-01-10 14:48:50 +0800

    test(integration_test): add empty test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,4 @@
 #[test]
 fn test_rib_server() {
-    use rib_server::message::Message;
-
-    let msg = Message::new("Alice".to_string(), "Hello world!".to_string());
-    println!("{}", msg);
-    assert_eq!(msg.name(), "Alice");
-    assert_eq!(msg.content(), "Hello world!");
+    todo!()
 }
